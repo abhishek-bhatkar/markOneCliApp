af3a16088da368108526e894eee594c86d8db98e
Abhishek Bhatkar <54368008+abhishek-bhatkar@users.noreply.github.com>
2021-08-26 05:49:16 +0000

    Add code for cli app

--- index.js
+++ index.js
@@ -1,7 +1,76 @@
-console.log("Abhishek Bhatkar");
+console.log("Quizz about Abhishek!");
+console.log("---#---#---#---");
+
+// declaring data
 
 var readLineSync = require('readline-sync');
 
-var userName = readLineSync.question("What is your name?");
-var welcomeMsg = "Welcome " + userName ;
-console.log("Welcome "+ userName);
+var score = 0;
+
+var questions = [
+  {
+    question: "Where do I live? ",
+    answer: "Ratnagiri"
+  }, 
+  {
+    question: "My favourite sport? ",
+    answer: "Cricket"
+  },
+  {
+    question: "Where do I work? ",
+    answer: "Athenahealth"
+  },
+  {
+    question: "What is my age? ",
+    answer: "24"
+  }
+];
+
+// welcome function
+function welcome() {
+userName = readLineSync.question("What's your name? ");
+
+console.log("Welcome "+ userName + " to quizz!");
+}
+
+// play function
+function play(question, answer) {
+  var userAnswer = readLineSync.question(question);
+
+  if (userAnswer.toUpperCase() === answer.toUpperCase()) { // branching
+    console.log("right!");
+    score = score + 1;
+    
+  } else {
+    console.log("Wrong answer!");
+   
+  }
+
+  console.log("current score: ", score);
+  console.log("---#---#---#---");
+}
+
+// game function
+function game() {
+  for (var i=0; i<questions.length; i++) {
+    var currentQuestion = questions[i];
+    play(currentQuestion.question, currentQuestion.answer)
+  }
+}
+
+// score function
+function showScores() {
+  console.log("YAY! You SCORED: ", score);
+
+}
+
+// calling functions
+
+welcome();
+game();
+showScores();
+
+
+
+
+
